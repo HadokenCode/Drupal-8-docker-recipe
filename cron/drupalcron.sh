0dc4d23802fcbbdf4fd12459772ab017acae5659
Madalin Ignisca <madalin@Madalins-MBP.home>
2016-07-01 12:10:23 +0100

    Basic cron, investigating if it is really working.

--- cron/drupalcron.sh
+++ cron/drupalcron.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-if [-f /code/vendor/bin/drupal]; then
+if [ ! -f /home/www-data/.console/config.yml ]; then
+    cd /code/web
+    ../vendor/bin/drupal init
+fi
+if [ -f /code/vendor/bin/drupal ]; then
     cd /code/web
     ../vendor/bin/drupal cron:execute all
 fi
